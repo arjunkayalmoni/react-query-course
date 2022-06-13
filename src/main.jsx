e76fabdbf30323a0365064859af29d206247c43c
Arjun KayalMoni <arjun.umathanu@gmail.com>
2022-06-13 11:38:14 +0530

    Added GithubUser Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { worker } from "@uidotdev/react-query-api";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+const client = new QueryClient();
 
 new Promise((res) => setTimeout(res, 100))
-  .then(() =>
-    worker.start({
-      quiet: true,
-      onUnhandledRequest: "bypass",
-    })
-  )
-  .then(() => {
-    ReactDOM.render(
-      <React.StrictMode>
-        <BrowserRouter>
-          <div className="container">
-            <App />
-          </div>
-        </BrowserRouter>
-      </React.StrictMode>,
-      document.getElementById("root")
-    );
-  });
+	.then(() =>
+		worker.start({
+			quiet: true,
+			onUnhandledRequest: "bypass",
+		})
+	)
+	.then(() => {
+		root.render(
+			<React.StrictMode>
+				<QueryClientProvider client={client}>
+					<BrowserRouter>
+						<div className="container">
+							<App />
+						</div>
+					</BrowserRouter>
+				</QueryClientProvider>
+			</React.StrictMode>
+		);
+	});
