35d22040fc1105514ac441e31e4247610b244aa9
Arjun KayalMoni <arjun.umathanu@gmail.com>
2022-06-14 11:49:41 +0530

    Added Issues Item and List components

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,35 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import axios from "axios";
+import IssueItem from "./IssueItem";
 
 export default function IssuesList() {
-  return (
-    <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
-    </div>
-  );
+	const issuesQuery = useQuery(["issues"], () =>
+		axios.get("/api/issues").then((res) => res.data)
+	);
+	return (
+		<div>
+			<h2>Issues List</h2>
+			{issuesQuery.isLoading ? (
+				<p>Loading.....</p>
+			) : (
+				<ul className="issues-list">
+					{/* <IssueItem key={ issue.id } { ...issue } /> */}
+					{issuesQuery.data.map((issue) => (
+						<IssueItem
+							key={issue.id}
+							title={issue.title}
+							number={issue.number}
+							assignee={issue.assignee}
+							commentCount={issue.comments.length}
+							createdBy={issue.createdBy}
+							createdDate={issue.createdDate}
+							labels={issue.labels}
+							status={issue.status}
+						/>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 }
