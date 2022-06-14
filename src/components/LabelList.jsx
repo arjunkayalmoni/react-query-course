9e4a97ebf7592b66715aa24e3694c109c6578e66
Arjun KayalMoni <arjun.umathanu@gmail.com>
2022-06-14 11:50:04 +0530

    Added Labels component

--- src/components/LabelList.jsx
+++ src/components/LabelList.jsx
@@ -1,3 +1,33 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const labelsUrl = "https://ui.dev/api/courses/react-query/labels";
+
 export default function LabelList() {
-  return <h3>Labels</h3>;
+	const labelsQuery = useQuery(["labels"], () =>
+		axios.get(labelsUrl).then((res) => res.data)
+	);
+
+	return (
+		<div>
+			<h3>Labels</h3>
+			{labelsQuery.isLoading && <p>Loading.....</p>}
+			{labelsQuery.isError && (
+				<p style={{ color: "red" }}>
+					Error: {labelsQuery.error.message}
+				</p>
+			)}
+
+			{labelsQuery.isSuccess && (
+				<ul>
+					{labelsQuery.data.map((label) => (
+						<li key={label.id} className="labels">
+							<span style={{ color: label.color }}></span>
+							{label.name}
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
 }
